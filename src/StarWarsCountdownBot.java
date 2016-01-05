e7f5b4eeb9280b53db61376c3c1f7f74fc7f6f48
jvasco <jordyvasco@gmail.com>
2016-01-05 17:33:34 -0500

    .

--- src/StarWarsCountdownBot.java
+++ src/StarWarsCountdownBot.java
@@ -12,7 +12,7 @@ public class StarWarsCountdownBot {
 	public static void main(String[] args) throws TwitterException,
 			InterruptedException, ParseException {
 		Twitter twitter = TwitterFactory.getSingleton();
-		int i = 70;
+		int i = 80;
 		while (true) {
 			SimpleDateFormat sdf = new SimpleDateFormat("ddMMyyyy");
 			int temp = daysBetween(Calendar.getInstance().getTime(),
