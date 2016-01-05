557ec800a40c4cf24050922710cdcfa147b868ef
jvasco <jordyvasco@gmail.com>
2016-01-05 01:02:33 -0500

    initial commit

---
+++ src/StarWarsCountdownBot.java
@@ -0,0 +1,30 @@
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
+import twitter4j.Twitter;
+import twitter4j.TwitterException;
+import twitter4j.TwitterFactory;
+
+public class StarWarsCountdownBot {
+	public static void main(String[] args) throws TwitterException,
+			InterruptedException, ParseException {
+		Twitter twitter = TwitterFactory.getSingleton();
+		int i = 9;
+		while (true) {
+			SimpleDateFormat sdf = new SimpleDateFormat("ddMMyyyy");
+			int temp = daysBetween(Calendar.getInstance().getTime(),
+					sdf.parse("16122016"));
+			twitter.updateStatus(temp + " days until #RogueOne! " + i);
+			i++;
+			Thread.sleep(8000);
+		}
+
+	}
+
+	private static int daysBetween(Date d1, Date d2) {
+		// TODO Auto-generated method stub
+		return (int) ((d2.getTime() - d1.getTime()) / (1000 * 60 * 60 * 24));
+	}
+}
