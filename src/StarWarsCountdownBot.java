a89b303d7a59d4e4c2a8aa16d431ca67556cfe97
jvasco <jordyvasco@gmail.com>
2016-01-05 16:34:55 -0500

    m

--- src/StarWarsCountdownBot.java
+++ src/StarWarsCountdownBot.java
@@ -1,24 +1,19 @@
-import twitter4j.TwitterFactory;
-import twitter4j.TwitterException;
-
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
+
 import twitter4j.Twitter;
 
 
 public class StarWarsCountdownBot {
-	public static void main(String[] args) throws TwitterException,
-			InterruptedException, ParseException {
-		Twitter twitter = TwitterFactory.getSingleton();
-		int i = 14;
+	public static void main(String[] args) throws InterruptedException {
+
+		int i = 15;
 		while (true) {
-			SimpleDateFormat sdf = new SimpleDateFormat("ddMMyyyy");
-			int temp = daysBetween(Calendar.getInstance().getTime(),
-					sdf.parse("16122016"));
-			twitter.updateStatus(temp + " days until #RogueOne! " + i);
+			System.out.println(i);
 			i++;
+			Twitter.class.getClass();
 			Thread.sleep(8000);
 		}
 
