b1001ba9336e67de39f461c786dd5b47d0010da1
jvasco <jordyvasco@gmail.com>
2016-01-05 21:47:18 -0500

    It's working!

--- src/StarWarsCountdownBot.java
+++ src/StarWarsCountdownBot.java
@@ -1,3 +1,6 @@
+import twitter4j.Query;
+import twitter4j.QueryResult;
+import twitter4j.Status;
 import twitter4j.TwitterFactory;
 import twitter4j.TwitterException;
 
@@ -5,21 +8,38 @@ import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
+
 import twitter4j.Twitter;
 
 
 public class StarWarsCountdownBot {
-	public static void main(String[] args) throws TwitterException,
-			InterruptedException, ParseException {
+	public static void main(String[] args) throws 
+			InterruptedException, ParseException, TwitterException {
 		Twitter twitter = TwitterFactory.getSingleton();
-		int i = 90;
+		Calendar cal = Calendar.getInstance();
 		while (true) {
 			SimpleDateFormat sdf = new SimpleDateFormat("ddMMyyyy");
 			int temp = daysBetween(Calendar.getInstance().getTime(),
 					sdf.parse("16122016"));
-			twitter.updateStatus(temp + " Now this is podracing! " + Math.random()*10);
-			i++;
-			Thread.sleep(8000);
+			int hour = cal.get(Calendar.HOUR_OF_DAY);
+			if (hour==12)
+			{
+			try {
+				twitter.updateStatus(temp + " days until #RogueOne!");
+			} catch (TwitterException e) {
+				// TODO Auto-generated catch block
+			}
+			}
+		Query query = new Query("@SW_Countdown");
+		QueryResult result;
+		result = twitter.search(query);
+		Status tweetResult = result.getTweets().get(0);
+		try {
+			twitter.updateStatus(".@" + tweetResult.getUser().getScreenName() + " It's working. It's working!!!");
+		} catch (TwitterException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		}
 
 	}
